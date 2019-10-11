d646a1f7af71677cee0c81d4f376187c3c165fe4
Sharath <sharusharath44@gmail.com>
2019-10-11 14:23:10 +0530

    initial commit

---
+++ app/src/androidTest/java/com/projects/sharath/chatbotv3/ExampleInstrumentedTest.java
@@ -0,0 +1,29 @@
+/*
+package com.projects.sharath.chatbotv3;
+
+import android.content.Context;
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+*/
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ *//*
+
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.projects.sharath.chatbotv3", appContext.getPackageName());
+    }
+}
+*/
